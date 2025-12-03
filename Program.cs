8d9258f04bcac9ac43635720f2aea5d717908af7
Mario J. Schwaiger <mario.schwaiger@v2c2.at>
2025-12-03 16:30:02 +0100

    Läuft

--- Program.cs
+++ Program.cs
@@ -30,50 +30,5 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/parkingSpots", async (ParkingLotDB db) =>
     await db.ParkingLots.ToListAsync());
 
-app.MapGet("/parkingSpot/Totalspots", async (ParkingLotDB db) =>
-    await db.ParkingLots.Where(t => t.getSpots).ToListAsync());
-
-app.MapGet("/parkingSpot/FreeSpots", async (ParkingLotDB db) =>
-    await db.ParkingLots.Where(t => t.getFreeSpots).ToListAsync());    
-
-app.MapGet("/parkingSpots/{id}", async (int id, ParkingLotDB db) =>
-    await db.ParkingLots.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
-
-app.MapPost("/todoitems", async (Todo todo, ParkingLotDB db) =>
-{
-    db.ParkingLots.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todoitems/{todo.Id}", todo);
-});
-
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, ParkingLotDB db) =>
-{
-    var todo = await db.ParkingLots.FindAsync(id);
-
-    if (todo is null) return Results.NotFound();
-
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/todoitems/{id}", async (int id, ParkingLotDB db) =>
-{
-    if (await db.ParkingLots.FindAsync(id) is Todo todo)
-    {
-        db.ParkingLots.Remove(todo);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-
-    return Results.NotFound();
-});
 
 app.Run();
